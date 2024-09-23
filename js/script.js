ced26b2ec1c563b45a013ea8d3517ca52f811203
ABDELFADEIL <abdalfadeil@gmail.com>
2024-09-23 13:58:31 +0200

    yaml file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
+const host = "https://abdelfadeil.github.io/app/";
+let data;
+// استدعاء الدالة وتحميل البيانات عند تحميل الصفحة
+window.onload = function () {
+    loadYAMLFile(host + 'data/data.yaml').then(response => {
+        if (response) {
+            console.log(response); // طباعة البيانات المحملة في وحدة التحكم
+            showLoadedNews(response);
+            showLoadedArticles(response);
+        }
+    });
+};
+
+// دالة لتحميل وتحويل ملف YAML إلى JSON
+async function loadYAMLFile(url ) {
+    try {
+        // استخدام fetch لتحميل ملف YAML
+        const response = await fetch(url);
+
+        // التحقق من أن الطلب كان ناجحاً
+        if (!response.ok) {
+            throw new Error(`Error fetching the YAML file: ${response.statusText}`);
+        }
+
+        // قراءة النص من الرد
+        const yamlText = await response.text();
+
+        // تحويل YAML إلى JSON باستخدام js-yaml
+        const jsonData = jsyaml.load(yamlText);
+        console.log(jsonData);
+        data = jsonData;
+        return jsonData; // إرجاع البيانات ككائن JSON
+    } catch (error) {
+        console.error("Failed to load YAML file:", error);
+        return null;
+    }
+}
 
 function navigateToPage(pageUrl) {
     console.log(pageUrl);
@@ -7,7 +44,6 @@ function navigateToPage(pageUrl) {
 
 
 document.addEventListener('DOMContentLoaded', function () {
-
 
     document.body.setAttribute('dir', 'rtl');
     const burger = document.getElementById('burger');
@@ -82,153 +118,126 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-// استخراج معرف المقال من الـ URL
-let urlParams = new URLSearchParams(window.location.search);
-let articleId = urlParams.get('id');
-
-// طباعة المعرف للتحقق
-console.log('معرف المقال:', articleId);
-
 // استخدام المعرف لجلب المحتوى المناسب بناءً على معرف المقال
 // يمكنك الآن عرض تفاصيل المقال بناءً على المعرف
 
-// الحصول على المسار الحالي للصفحة
-const currentPath = window.location.pathname;
 
-// تحديد المسار الصحيح لملف JSON بناءً على الصفحة
-let jsonFilePath;
-if (currentPath.includes('article-details.html')) {
-    // إذا كانت الصفحة هي صفحة تفاصيل المقال
-    jsonFilePath = 'https://abdelfadeil.github.io/app/data/articles.json';
-} else {
-    // إذا كانت الصفحة هي الصفحة الرئيسية أو صفحة أخرى
-    jsonFilePath = '../app/data/articles.json';
+function showLoadedArticles(data) {
+    const articles = data.articles;
+    const articlesContainer = document.getElementById('articles-section');
+
+    // إنشاء العناصر الخاصة بالمقالات
+    articles.forEach(article => {
+        const articleItem = document.createElement('article');
+        articleItem.classList.add('article-item');
+        console.log(article.id);
+        articleItem.setAttribute('data-article-id', article.id);
+
+        // إضافة الصورة المصغرة
+        const articleThumbnail = document.createElement('img');
+        articleThumbnail.src = article.author.image;
+        console.log(article.author.image);
+        articleThumbnail.alt = article.title;
+        articleThumbnail.classList.add('article-thumbnail');
+
+        // إضافة محتوى المقال
+        const articleContent = document.createElement('div');
+        articleContent.classList.add('article-item-content');
+
+        const articleTitle = document.createElement('h3');
+        articleTitle.textContent = article.title;
+
+        articleContent.appendChild(articleTitle);
+        //
+        if (currentPath.includes('article-details.html')) {
+            // إذا كانت الصفحة هي صفحة تفاصيل المقال
+            const articleLink = document.createElement('a');
+            articleLink.href = `https://abdelfadeil.github.io/app/pages/article-details.html?id=${article.id}`;
+            articleLink.textContent = "اقرأ المزيد";
+            articleContent.appendChild(articleLink);
+        } else {
+            // إذا كانت الصفحة هي الصفحة الرئيسية أو صفحة أخرى
+            //articleLink.href = `https://abdelfadeil.github.io/app/pages/article-details.html?id=${article.id}`;
+            //articleLink.textContent = "اقرأ المزيد";
+            const articleParagraphe = document.createElement('p');
+            articleParagraphe.textContent = article.author.name;
+            articleContent.appendChild(articleParagraphe);
+            articleItem.classList.add('article-item-home-page');
+        }
+        //
+
+        // تجميع العناصر
+        articleItem.appendChild(articleThumbnail);
+        articleItem.appendChild(articleContent);
+
+        // إضافة المقال إلى الحاوية
+        articlesContainer.appendChild(articleItem);
+        // الحصول على جميع العناصر التي تحتوي على المقالات
+        const articleItems = document.querySelectorAll('.article-item-home-page');
+        console.log(articleItems.length);
+        // إضافة مستمع للنقر على كل عنصر article-item
+        articleItems.forEach(item => {
+            item.addEventListener('click', function () {
+                // استخراج معرف المقال من data-article-id
+                const articleId = item.getAttribute('data-article-id');
+                console.log("articleId", articleId);
+                // نقل الزائر إلى صفحة المقال بناءً على معرف المقال
+                window.location.href = `https://abdelfadeil.github.io/app/pages/article-details.html?id=${articleId}`;
+            });
+        });
+    });
 }
 
-// جلب المقالات من ملف JSON
-fetch("https://abdelfadeil.github.io/app/data/articles.json")
-    .then(response => response.json())
-    .then(data => {
-        const articles = data.articles;
-        const articlesContainer = document.getElementById('articles-section');
 
-        // إنشاء العناصر الخاصة بالمقالات
-        articles.forEach(article => {
-            const articleItem = document.createElement('article');
-            articleItem.classList.add('article-item');
-            console.log(article.id);
-            articleItem.setAttribute('data-article-id', article.id);
+function showLoadedNews(data) {
+    const news = data.news;
+    const newsCarousel = document.getElementById('news-carousel');
 
-            // إضافة الصورة المصغرة
-            const articleThumbnail = document.createElement('img');
-            articleThumbnail.src = article.author.image;
-            console.log(article.author.image);
-            articleThumbnail.alt = article.title;
-            articleThumbnail.classList.add('article-thumbnail');
+    // إنشاء العناصر الخاصة بالأخبار
+    news.forEach(newsItem => {
+        // إنشاء العنصر news-item
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('news-item');
+        newsDiv.setAttribute('data-news-id', newsItem.id);
 
-            // إضافة محتوى المقال
-            const articleContent = document.createElement('div');
-            articleContent.classList.add('article-item-content');
+        // إضافة صورة الخبر
+        const newsImage = document.createElement('img');
+        newsImage.src = newsItem.image;
+        newsImage.alt = newsItem.headline;
+        newsImage.classList.add('news-image');
 
-            const articleTitle = document.createElement('h3');
-            articleTitle.textContent = article.title;
+        // إنشاء محتوى الخبر
+        const newsContent = document.createElement('div');
+        newsContent.classList.add('news-item-content');
 
-            articleContent.appendChild(articleTitle);
-            // 
-            if (currentPath.includes('article-details.html')) {
-                // إذا كانت الصفحة هي صفحة تفاصيل المقال
-                const articleLink = document.createElement('a');
-                articleLink.href = `https://abdelfadeil.github.io/app/pages/article-details.html?id=${article.id}`;
-                articleLink.textContent = "اقرأ المزيد";
-                articleContent.appendChild(articleLink);
-            } else {
-                // إذا كانت الصفحة هي الصفحة الرئيسية أو صفحة أخرى
-                //articleLink.href = `https://abdelfadeil.github.io/app/pages/article-details.html?id=${article.id}`;
-                //articleLink.textContent = "اقرأ المزيد";
-                const articleParagraphe = document.createElement('p');
-                articleParagraphe.textContent = article.author.name;
-                articleContent.appendChild(articleParagraphe);
-                articleItem.classList.add('article-item-home-page');
-            }
-            //
+        const newsTitle = document.createElement('h3');
+        newsTitle.textContent = newsItem.headline;
 
-            // تجميع العناصر
-            articleItem.appendChild(articleThumbnail);
-            articleItem.appendChild(articleContent);
+        const newsText = document.createElement('p');
+        newsText.textContent = newsItem.summary;
 
-            // إضافة المقال إلى الحاوية
-            articlesContainer.appendChild(articleItem);
-            // الحصول على جميع العناصر التي تحتوي على المقالات
-            const articleItems = document.querySelectorAll('.article-item-home-page');
-            console.log(articleItems.length);
-            // إضافة مستمع للنقر على كل عنصر article-item
-            articleItems.forEach(item => {
-                item.addEventListener('click', function () {
-                    // استخراج معرف المقال من data-article-id
-                    const articleId = item.getAttribute('data-article-id');
-                    console.log("articleId", articleId);
-                    // نقل الزائر إلى صفحة المقال بناءً على معرف المقال
-                    window.location.href = `https://abdelfadeil.github.io/app/pages/article-details.html?id=${articleId}`;
-                });
-            });
+        // إضافة المحتوى للخبر
+        newsContent.appendChild(newsTitle);
+        newsContent.appendChild(newsText);
+
+        // تجميع العناصر
+        newsDiv.appendChild(newsImage);
+        newsDiv.appendChild(newsContent);
+
+        // إضافة الخبر إلى الحاوية
+        newsCarousel.appendChild(newsDiv);
+
+        // إضافة مستمع للنقر على محتوى الخبر لنقل المستخدم إلى صفحة تفاصيل الخبر
+        newsContent.addEventListener('click', () => {
+            window.location.href = `https://abdelfadeil.github.io/app/pages/news-details.html?id=${newsItem.id}`;
         });
-    })
-    .catch(error => console.error('خطأ في جلب المقالات:', error));
+    });
+}
 
-// جلب الأخبار من ملف JSON
-fetch('https://abdelfadeil.github.io/app/data/articles.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log('sssssssssss news ')
-        console.log(data);
-        const news = data.news;
-        const newsCarousel = document.getElementById('news-carousel');
-
-        // إنشاء العناصر الخاصة بالأخبار
-        news.forEach(newsItem => {
-            // إنشاء العنصر news-item
-            const newsDiv = document.createElement('div');
-            newsDiv.classList.add('news-item');
-            newsDiv.setAttribute('data-news-id', newsItem.id);
-
-            // إضافة صورة الخبر
-            const newsImage = document.createElement('img');
-            newsImage.src = newsItem.image;
-            newsImage.alt = newsItem.title;
-            newsImage.classList.add('news-image');
-
-            // إنشاء محتوى الخبر
-            const newsContent = document.createElement('div');
-            newsContent.classList.add('news-item-content');
-
-            const newsTitle = document.createElement('h3');
-            newsTitle.textContent = newsItem.title;
-
-            const newsText = document.createElement('p');
-            newsText.textContent = newsItem.content;
-
-            // إضافة المحتوى للخبر
-            newsContent.appendChild(newsTitle);
-            newsContent.appendChild(newsText);
-
-            // تجميع العناصر
-            newsDiv.appendChild(newsImage);
-            newsDiv.appendChild(newsContent);
-
-            // إضافة الخبر إلى الحاوية
-            newsCarousel.appendChild(newsDiv);
-
-            // إضافة مستمع للنقر على محتوى الخبر لنقل المستخدم إلى صفحة تفاصيل الخبر
-            newsContent.addEventListener('click', () => {
-                window.location.href = `https://abdelfadeil.github.io/app/pages/news-details.html?id=${newsItem.id}`;
-            });
-        });
-    })
-    .catch(error => console.error('خطأ في جلب الأخبار:', error));
 
 // تغيير الفيديو في نافذة الفيديوهات
     function changeVideo(videoID) {
         const mainVideoIframe = document.getElementById('main-video-iframe');
         mainVideoIframe.src = `https://www.youtube.com/embed/${videoID}`;
     }
-    
+
